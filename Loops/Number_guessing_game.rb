80003578ed7f056cd23cffe1fe6f426f0c145756
Robi <robi.maliqi@hotmail.co.uk>
2022-05-03 20:42:03 +0100

    Created number guessing game

---
+++ Loops/Number_guessing_game.rb
@@ -0,0 +1,28 @@
+# Number Guessing game
+
+
+# while loop; keeps looping until the user gets the correct number
+
+while ture
+    print "Enter a number 1 to 100: "
+# asks the user to input a number
+    number = rand(1..100)
+# generates a ranom number between 1 to 100
+    input = get.to_i
+# takes the input from the user and turns it into a intiger
+    if input < number 
+        puts "Enter a larger number"
+# if the number the user puts in(input) is less than (<) the computer
+# generates the computer will tell the user to put a larger number
+
+    elsif input > number
+        puts "Enter a smaller number"
+# if the number the user puts in(input) is more than (>) the computer
+# generates the computer will tell the user to put a larger number
+
+    elsif input == number
+        puts "Correct!"
+        break
+# once the user guesses the right number it tells the user and the loop stops
+    end
+end
